bc6a6ac500d5c3d5aaca23cba989d8c35073f7f2
Peter Jiang <peter.j@usask.ca>
2024-11-03 22:01:21 -0600

    JavaScript To-Do List

---
+++  Lab/Lab 6/starterCode/lab6.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const taskInput = document.getElementById("taskinput");
+    const addTaskButton = document.getElementById("addTaskButton");
+    const taskList = document.getElementById("taskList");
+});

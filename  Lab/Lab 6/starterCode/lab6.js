39b6f7aa37d549fd804e13d107aa49e7a70e0144
Peter Jiang <peter.j@usask.ca>
2024-11-03 22:02:23 -0600

    Created an event listener for the Add Task Button

---  Lab/Lab 6/starterCode/lab6.js
+++  Lab/Lab 6/starterCode/lab6.js
@@ -3,4 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const taskInput = document.getElementById("taskinput");
     const addTaskButton = document.getElementById("addTaskButton");
     const taskList = document.getElementById("taskList");
+
+    addTaskButton.addEventListener("click", function(){
+        
+    })
 });
